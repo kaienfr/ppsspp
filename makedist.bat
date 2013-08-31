02dfbfee69c7461995715491a718c2e1615e39c5
kaienfr <kaienfr@gmail.com>
2013-09-01 01:43:41 +0200

    modif makedist.bat

--- makedist.bat
+++ makedist.bat
@@ -1,9 +1,11 @@
 @echo off
+del /F /Q dist
 mkdir dist
 copy /Y PPSSPPWindows.exe dist\
 copy /Y PPSSPPWindows64.exe dist\
 xcopy /E /Y flash0 dist\flash0\
 xcopy /E /Y lang dist\lang\
-xcopy /E /Y assets dist\assets\
+xcopy /E /Y assets\ui_atlas.zim dist\assets\ui_atlas.zim
+xcopy /E /Y assets\ppge_atlas.zim dist\assets\ppge_atlas.zim
 echo 发布版安装成功，见dist目录
 pause
